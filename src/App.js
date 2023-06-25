a4cb4c1d0f67fcda4f556208bb0645299c1f2a70
shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-25 19:27:25 +0330

    lastSeenTerms page for student

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 import LoginForm from './components/LoginForm';
 import AllTerms from './components/AllTerms'
+import LastTerms from './components/LastTerms';
+
 function App() {
   const supervisor = {
     email: "admin@admin.com",
     password: "admin123"
+  }
+
+  const student = {
+    email: "stu@stu.com",
+    password: "stu123"
   }
 
   const [user, setUser] = useState({name: "", email: ""});
@@ -13,12 +20,19 @@ function App() {
   const Login = details => {
     console.log(details);
     if (details.email === supervisor.email && details.password === supervisor.password){
-      console.log("Logged In");
+      console.log("supervisor Logged In");
       setUser({
         name: details.name,
         email: details.email
       });
     } 
+    else if (details.email === student.email && details.password === student.password){
+      console.log("student Logged In");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }
     else{
       console.log("details don't match")
       setError("Details don't match")
@@ -27,12 +41,13 @@ function App() {
 
   return (
     <div className="App">
-      {(user.email !== "") ? (
+      {user.email === supervisor.email ? (
         <AllTerms />
+      ) : user.email === student.email ? (
+        <LastTerms />
       ) : (
         <LoginForm Login={Login} error={error}/>
-      )
-    }
+      )}
     </div>
   );
 }
---
+++ src/components/LastTerms.js
@@ -0,0 +1,85 @@
+import React, {useState} from "react";
+import CourseInfo from "./CourseInfo"; 
+
+function LastTerms(props){
+  const [numTerms, setNumTerms] = useState(6);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCourse, setSelectedCourse] = useState("");
+
+  const {selectedTerm, onClose} = props;
+
+  const see_more = () => {
+    console.log("see more");
+    setNumTerms(numTerms + 4)
+  }
+
+  const handleCourseClick = (courseName) => {
+    console.log("Clicked on course:", courseName);
+    setSelectedCourse(courseName);
+  }
+
+  const courses = [
+    "مبانی برنامه‌نویسی",
+    "برنامه‌نویسی پیشرفته",
+    "ساختمان داده",
+    "نظریه زبان‌ها و  ماشین‌ها",
+    "مدارهای الکتریکی",
+    " مدار منطقی",
+    "آمار و احتمال",
+    "معماری کامپیوتر",
+    "شبکه‌های کامپیوتری",
+    "سیستم عامل",
+    "گرافیک کامپیوتری",
+    "هوش مصنوعی",
+  ];
+
+  const filteredCourses = courses.filter((course) =>
+    course.includes(searchQuery)
+  );
+
+  if (selectedCourse) {
+    return <CourseInfo selectedTerm={selectedTerm} selectedCourse={selectedCourse} onClose={onClose} />;
+  }
+
+  return (
+    <div className='terms-container'> 
+      <div className='left'>
+        <div className='term-bar'>
+          <h2> خروج </h2>
+          <h2>علیرضا کریمی</h2>
+        </div>
+        <div className="home">خانه</div>
+        <hr className="line"></hr>
+        <div className="last-seen-term">آخرین ترم مشاهده شده</div>
+        <div className="last-term">
+            ترم بهار 1402
+        </div>
+        <div className="last-seen-courses">آخرین دروس مشاهده شده</div>
+        <hr className="half-line"></hr>
+        <div className='terms'>
+          {filteredCourses.slice(0, numTerms).map((course) => (
+            <div
+              className="term-item"
+              onClick={() => handleCourseClick(course)}
+              key={course}
+            >
+              {course}
+            </div>
+          ))}
+        </div>
+        {numTerms < filteredCourses.length && (
+          <button onClick={see_more} className="see-more">
+            مشاهده بیشتر
+          </button>
+        )}
+      </div>
+      {
+        <div className='terms-list-right'>
+          مشاهده لیست ترم‌ها
+        </div>
+      }
+    </div>
+  )
+}
+
+export default LastTerms 
