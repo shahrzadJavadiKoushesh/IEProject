e3c9c96f021504c4233c53ede122cf5e77e693ad
shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-25 14:53:53 +0330

    add searchbar for supervisor

--- src/components/TermsInfo.js
+++ src/components/TermsInfo.js
@@ -2,57 +2,73 @@ import React, {useState} from "react";
 
 function TermsInfo(props){
   const [numTerms, setNumTerms] = useState(6);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const {selectedTerm, onClose} = props;
 
-    const see_more = () => {
-        console.log("see more");
-        setNumTerms(numTerms + 4)
-    }
+  const see_more = () => {
+    console.log("see more");
+    setNumTerms(numTerms + 4)
+  }
 
-    const goNextPage = () => {
-      console.log("CLICKED");
-    }
+  const goNextPage = () => {
+    console.log("CLICKED");
+  }
 
-    const courses = [
-      "مبانی برنامه‌نویسی",
-      "برنامه‌نویسی پیشرفته",
-      "ساختمان داده",
-      "نظریه زبان‌ها و  ماشین‌ها",
-      "مدارهای الکتریکی",
-      " مدار منطقی",
-      "مبانی برنامه‌نویسی",
-      "برنامه‌نویسی پیشرفته",
-      "ساختمان داده",
-      "نظریه زبان‌ها و  ماشین‌ها",
-      "مدارهای الکتریکی",
-      " مدار منطقی",
-    ];
-    return (
-        <div className='terms-container'> 
-          <div className='left'>
-            <div className='term-bar'>
-              <h2>مشاهده اطلاعات ترم</h2>
-              <h2>دروس {selectedTerm}</h2>
-            </div>
-            <div className='terms'>
-              {courses.slice(0, numTerms).map((course) =>(
-                <div className="term-item" onClick={goNextPage}>
-                  {course}
-                </div>
-              ))}
-            </div>
-            {numTerms < courses.length && (
-                <button onClick={see_more} className="see-more">مشاهده بیشتر</button>
-              )}
-          </div>
-          {
-            <div className='terms-list-right'>
-              مشاهده لیست ترم‌ها
-            </div>
-          }
+  const courses = [
+    "مبانی برنامه‌نویسی",
+    "برنامه‌نویسی پیشرفته",
+    "ساختمان داده",
+    "نظریه زبان‌ها و  ماشین‌ها",
+    "مدارهای الکتریکی",
+    " مدار منطقی",
+    "آمار و احتمال",
+    "معماری کامپیوتر",
+    "شبکه‌های کامپیوتری",
+    "سیستم عامل",
+    "گرافیک کامپیوتری",
+    "هوش مصنوعی",
+  ];
+
+  const filteredCourses = courses.filter((course) =>
+    course.includes(searchQuery)
+  );
+
+  return (
+    <div className='terms-container'> 
+      <div className='left'>
+        <div className='term-bar'>
+          <h2>مشاهده اطلاعات ترم</h2>
+          <h2>دروس {selectedTerm}</h2>
         </div>
-    )
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="جستجوی دوره‌ها"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <div className='terms'>
+          {filteredCourses.slice(0, numTerms).map((course) => (
+            <div className="term-item" onClick={goNextPage}>
+              {course}
+            </div>
+          ))}
+        </div>
+        {numTerms < filteredCourses.length && (
+          <button onClick={see_more} className="see-more">
+            مشاهده بیشتر
+          </button>
+        )}
+      </div>
+      {
+        <div className='terms-list-right'>
+          مشاهده لیست ترم‌ها
+        </div>
+      }
+    </div>
+  )
 }
 
 export default TermsInfo
